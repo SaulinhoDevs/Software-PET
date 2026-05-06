886dd5e8e5265e0421a36ea95be9bc0402e3e353
SaulinhoDevs <157417082+SaulinhoDevs@users.noreply.github.com>
2026-05-06 14:55:22 -0300

    Add Spring Boot no projeto backend

---
+++ backend/buscaativa/src/main/java/com/pet/buscaativa/BuscaativaApplication.java
@@ -0,0 +1,13 @@
+package com.pet.buscaativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscaativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscaativaApplication.class, args);
+	}
+
+}
---
+++ backend/buscaativa/src/test/java/com/pet/buscaativa/BuscaativaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.buscaativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscaativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
