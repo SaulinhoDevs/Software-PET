675226ed61fcd22c6f0f38d7ee69b4d7280a0186
SaulinhoDevs <157417082+SaulinhoDevs@users.noreply.github.com>
2026-07-07 18:29:50 -0300

    Atualização no config do angular para buscar os dados da API

--- frontend/sentinela-pet/src/app/app.config.ts
+++ frontend/sentinela-pet/src/app/app.config.ts
@@ -1,11 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+
 import { routes } from './app.routes';
+import { meuhttpInterceptor } from './services/http-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+
+    provideRouter(routes),
+
+    provideHttpClient(withInterceptors([meuhttpInterceptor])),
+  ],
 };
