3cc0ccda929542ff1585509b5787a3fa17ca8273
Liz <124801714+LizReis@users.noreply.github.com>
2026-07-09 18:04:35 +0000

    Add funcionalidades do calculo de vagas, add colocação de falta para o paciente caso a situação do atendimento seja FALTOU

--- backend/buscaativa/src/main/java/com/pet/buscaativa/services/impl/AgendamentoServiceImpl.java
+++ backend/buscaativa/src/main/java/com/pet/buscaativa/services/impl/AgendamentoServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import com.pet.buscaativa.entities.Agendamento;
 import com.pet.buscaativa.entities.Disponibilidade;
+import com.pet.buscaativa.entities.Paciente;
 import com.pet.buscaativa.entities.Usuario;
 import com.pet.buscaativa.entities.dto.AgendamentoDTO;
 import com.pet.buscaativa.entities.enums.SituacaoAtendimento;
@@ -19,10 +20,12 @@ import com.pet.buscaativa.mapping.AgendamentoMapper;
 import com.pet.buscaativa.repositories.AgendamentoRepository;
 import com.pet.buscaativa.repositories.BloqueioAgendaRepository;
 import com.pet.buscaativa.repositories.DisponibilidadeRepository;
+import com.pet.buscaativa.repositories.PacienteRepository;
 import com.pet.buscaativa.repositories.UsuarioRepository;
 import com.pet.buscaativa.services.AgendamentoService;
 import com.pet.buscaativa.services.exceptions.ResourceNotFoundException;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -31,9 +34,11 @@ public class AgendamentoServiceImpl implements AgendamentoService{
 
 
     private final AgendamentoRepository agendamentoRepository;
-    private final BloqueioAgendaRepository bloqueioRepository;
+    private final BloqueioAgendaRepository bloqueioAgendaRepository;
     private final UsuarioRepository usuarioRepository;
     private final DisponibilidadeRepository disponibilidadeRepository;
+    private final PacienteRepository pacienteRepository;
+
     private final AgendamentoMapper agendamentoMapper;
 
 
@@ -66,14 +71,39 @@ public class AgendamentoServiceImpl implements AgendamentoService{
 
     @Override
     public int calcularVagasDisponiveis(Long usuarioId, LocalDate data) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'calcularVagasDisponiveis'");
+        Usuario usuario = usuarioRepository.findById(usuarioId)
+                .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado: " + usuarioId));
+
+        // Se a data cai em feriado ou data bloqueada a disponibilidade é ZERO
+        boolean isBloqueado = bloqueioAgendaRepository.isDataBloqueadaParaUsuario(usuario, data);
+        if (isBloqueado) {
+            return 0; 
+        }
+
+        DayOfWeek diaSemana = data.getDayOfWeek();
+        int vagasTotais = 0;
+
+        // Busca a disponibilidade da Manhã
+        Optional<Disponibilidade> manhaOpt = disponibilidadeRepository.findByUsuarioAndDiaDaSemanaAndTurno(usuario, diaSemana, TurnoEnum.MANHA);
+        if (manhaOpt.isPresent()) {
+            int ocupadas = agendamentoRepository.contarVagasOcupadas(usuario, data, TurnoEnum.MANHA, SituacaoAtendimento.FALTOU);
+            vagasTotais += Math.max(0, manhaOpt.get().getCapacidade() - ocupadas);
+        }
+
+        // Busca a disponibilidade da Tarde
+        Optional<Disponibilidade> tardeOpt = disponibilidadeRepository.findByUsuarioAndDiaDaSemanaAndTurno(usuario, diaSemana, TurnoEnum.TARDE);
+        if (tardeOpt.isPresent()) {
+            int ocupadas = agendamentoRepository.contarVagasOcupadas(usuario, data, TurnoEnum.TARDE, SituacaoAtendimento.FALTOU);
+            vagasTotais += Math.max(0, tardeOpt.get().getCapacidade() - ocupadas);
+        }
+
+        return vagasTotais;
     }
 
     @Override
     public List<AgendamentoDTO> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+        var list = agendamentoRepository.findAll();
+        return list.stream().map(agendamentoMapper::toAgendamentoDTO).toList();
     }
 
     @Override
@@ -89,7 +119,7 @@ public class AgendamentoServiceImpl implements AgendamentoService{
 
             dataVerificacao = dataVerificacao.plusDays(1); 
 
-            boolean isBloqueado = bloqueioRepository.isDataBloqueadaParaUsuario(usuario, dataVerificacao);
+            boolean isBloqueado = bloqueioAgendaRepository.isDataBloqueadaParaUsuario(usuario, dataVerificacao);
             if(isBloqueado){
                 continue;
             }
@@ -142,11 +172,25 @@ public class AgendamentoServiceImpl implements AgendamentoService{
     }
 
     @Override
+    @Transactional
     public AgendamentoDTO atualizarStatus(Long id, SituacaoAtendimento novoStatus) {
         Agendamento agendamento = agendamentoRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Agendamento não encontrado para o id: " + id));
+                .orElseThrow(() -> new ResourceNotFoundException("Agendamento não encontrado para o id"));
 
         agendamento.setSituacaoAtendimento(novoStatus);
+
+        Paciente paciente = agendamento.getPaciente();
+        if (paciente != null) {
+            if (novoStatus == SituacaoAtendimento.FALTOU) {
+                paciente.setCountFaltas(paciente.getCountFaltas() + 1);
+            } else if (novoStatus == SituacaoAtendimento.PRESENTE) {
+                paciente.setCountFaltas(0);
+                paciente.setDataUltimaPresenca(LocalDate.now());
+            }
+            // salva paciente após receber falta ou returar falta ao atualizar o status do agendamento
+            pacienteRepository.save(paciente);
+        }
+
         agendamento = agendamentoRepository.save(agendamento);
 
         return agendamentoMapper.toAgendamentoDTO(agendamento);
